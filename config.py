5671f5071d1ac701d00c34bcb10f4c4a350b7324
Natallia <kotnemets5@mail.ru>
2024-10-12 15:51:05 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = '7531960131:AAGVoS1YNXq_fCVkjhPDcybHX0vgw2Xp3HQ'  # создаём констант
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETH',
+    'доллар': 'USD',
+}
---
+++ config1.py
@@ -0,0 +1,7 @@
+TOKEN = '8186267595:AAHuMfszkZ8O0jiEDupnVEYNi4ETueiaAbI'
+
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+}
